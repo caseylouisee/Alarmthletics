26d0b28ae291c461b34b16e5c332ba1767b7f6fe
Casey Denner <caseylouisee@me.com>
2017-03-30 12:11:38 +0100

    First Commit

---
+++ app/src/main/java/com/example/caseydenner/alarmthletics/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.caseydenner.alarmthletics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    TimePicker time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        time = (TimePicker)findViewById(R.id.timePicker);
+
+    }
+
+
+
+}
